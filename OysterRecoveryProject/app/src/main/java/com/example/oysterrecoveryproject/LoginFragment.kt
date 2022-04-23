1dcd2d150ebfe5d4f4f30e1302dd72ba2a1d1b5a
mikeo9911 <70450197+mikeo9911@users.noreply.github.com>
2022-04-22 20:53:23 -0400

    firebase changes

--- OysterRecoveryProject/app/src/main/java/com/example/oysterrecoveryproject/LoginFragment.kt
+++ OysterRecoveryProject/app/src/main/java/com/example/oysterrecoveryproject/LoginFragment.kt
@@ -1,6 +1,8 @@
 package com.example.oysterrecoveryproject
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -8,10 +10,12 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
 
 class LoginFragment : Fragment() {
     private lateinit var mUsername: EditText
     private lateinit var mPassword: EditText
+    private var mAuth: FirebaseAuth? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,6 +23,8 @@ class LoginFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         var view = inflater.inflate(R.layout.fragment_login, container, false)
+
+        mAuth = FirebaseAuth.getInstance()
 
         mUsername = view.findViewById(R.id.login_username)
         mPassword = view.findViewById(R.id.login_password)
@@ -36,17 +42,37 @@ class LoginFragment : Fragment() {
     }
 
     private fun loginUser() {
-        when {
-            mUsername.text.isEmpty() -> {
-                mUsername.error = "Please enter a username"
-            }
-            mPassword.text.isEmpty() -> {
-                mPassword.error = "Please enter a password"
-            }
-            mUsername.text.toString().isNotEmpty() && mPassword.text.toString().isNotEmpty() -> {
-                Toast.makeText(context, "Login Successful", Toast.LENGTH_SHORT).show()
-            }
+        val email: String = mUsername.text.toString()
+        val password: String = mPassword.text.toString()
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(context, "Please enter email...", Toast.LENGTH_LONG).show()
+            return
         }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(context, "Please enter password!", Toast.LENGTH_LONG).show()
+            return
+        }
+//        when {
+//            mUsername.text.isEmpty() -> {
+//                mUsername.error = "Please enter a username"
+//            }
+//            mPassword.text.isEmpty() -> {
+//                mPassword.error = "Please enter a password"
+//            }
+//            mUsername.text.toString().isNotEmpty() && mPassword.text.toString().isNotEmpty() -> {
+//                Toast.makeText(context, "Login Successful", Toast.LENGTH_SHORT).show()
+//            }
+//        }
+        mAuth!!.signInWithEmailAndPassword(email, password)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    Toast.makeText(context, "Login Successful", Toast.LENGTH_SHORT).show()
+                    //startActivity(Intent(this@LoginActivity, DashboardActivity::class.java))
+                } else {
+                    Toast.makeText(context,"Login failed! Please try again later", Toast.LENGTH_SHORT).show()
+                }
+            }
     }
 
 
--- OysterRecoveryProject/app/src/main/java/com/example/oysterrecoveryproject/RegisterFragment.kt
+++ OysterRecoveryProject/app/src/main/java/com/example/oysterrecoveryproject/RegisterFragment.kt
@@ -1,13 +1,14 @@
 package com.example.oysterrecoveryproject
 
+import android.content.Intent
 import android.os.Bundle
-import android.text.TextUtils
 import android.util.Patterns
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.*
+import com.google.firebase.auth.FirebaseAuth
 
 class RegisterFragment : Fragment() {
     private lateinit var mRadioGroup: RadioGroup
@@ -15,6 +16,9 @@ class RegisterFragment : Fragment() {
     private lateinit var mUsername: EditText
     private lateinit var mPassword: EditText
     private lateinit var mConfirmPassword: EditText
+    //private lateinit var progressBar: ProgressBar
+
+    private var mAuth: FirebaseAuth? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,6 +31,8 @@ class RegisterFragment : Fragment() {
         mUsername = view.findViewById(R.id.register_username)
         mPassword = view.findViewById(R.id.register_password)
         mConfirmPassword = view.findViewById(R.id.register_confirm_password)
+
+        mAuth = FirebaseAuth.getInstance()
 
         view.findViewById<Button>(R.id.back_to_login_button).setOnClickListener {
             var navRegister = activity as FragNav
@@ -48,30 +54,86 @@ class RegisterFragment : Fragment() {
     }
 
     private fun registerNewUser() {
-        when {
-            mUsername.text.isEmpty() -> {
-                mUsername.error = "Please enter a username"
-            }
-            mPassword.text.isEmpty() -> {
-                mPassword.error = "Please enter a password"
-            }
-            mConfirmPassword.text.isEmpty() -> {
-                mConfirmPassword.error = "Please confirm password"
-            }
+        //progressBar.visibility = View.VISIBLE
 
-            // add logic for radio buttons
+        val email: String = mUsername.text.toString()
+        val password: String = mPassword.text.toString()
+        val confpassword: String = mConfirmPassword.text.toString()
 
-            mUsername.text.toString().isNotEmpty() && mPassword.text.toString().isNotEmpty() &&
-                    mConfirmPassword.text.toString().isNotEmpty() -> {
+        if (email.isEmpty()) {
+            mUsername.error = "Please enter a username"
+            return
+        }
+        if (password.isEmpty()) {
+            mPassword.error = "Please enter a password"
+            return
+        }
+        if (confpassword.isEmpty()) {
+            mConfirmPassword.error = "Please confirm password"
+            return
+        }
+        if (!Patterns.EMAIL_ADDRESS.matcher(mUsername.text.toString()).matches()) {
+            mUsername.error = "Please enter a valid email address"
+            return
+        }
+        if (mPassword.text.toString() != mConfirmPassword.text.toString()) {
+            mConfirmPassword.error = "passwords do not match"
+            return
+        }
+        val x = mAuth!!.createUserWithEmailAndPassword(email, password)
 
-                if (!Patterns.EMAIL_ADDRESS.matcher(mUsername.text.toString()).matches()) {
-                    mUsername.error = "Please enter a valid email address"
-                } else if (mPassword.text.toString() != mConfirmPassword.text.toString()) {
-                     mConfirmPassword.error = "passwords do not match"
-                } else {
-                    Toast.makeText(context, "You are registered!", Toast.LENGTH_SHORT).show()
-                }
-                    }
+        x.addOnCompleteListener { task ->
+            //progressBar.visibility = View.GONE
+            if (task.isSuccessful) {
+                Toast.makeText(context, "You are registered!", Toast.LENGTH_SHORT).show()
+                var navRegister = activity as FragNav
+                navRegister.navigateFrag(LoginFragment(), false)
+                //startActivity(Intent(this@RegistrationActivity, LoginActivity::class.java))
+            } else {
+                Toast.makeText(context, "Registration failed", Toast.LENGTH_LONG).show()
+            }
         }
     }
 }
+//        when {
+
+//            email.isEmpty() -> {
+//                mUsername.error = "Please enter a username"
+//            }
+//            mPassword.text.isEmpty() -> {
+//                mPassword.error = "Please enter a password"
+//            }
+//            mConfirmPassword.text.isEmpty() -> {
+//                mConfirmPassword.error = "Please confirm password"
+//            }
+
+            // add logic for radio buttons
+
+//            mUsername.text.toString().isNotEmpty() && mPassword.text.toString().isNotEmpty() &&
+//                    mConfirmPassword.text.toString().isNotEmpty() -> {
+//
+//                val x = mAuth!!.createUserWithEmailAndPassword(mUsername.text.toString(), mPassword.text.toString())
+//
+//                x.addOnCompleteListener { task ->
+//                    progressBar.visibility = View.GONE
+//                    if (task.isSuccessful) {
+//                        Toast.makeText(context, "You are registered!", Toast.LENGTH_SHORT).show()
+//                        var navRegister = activity as FragNav
+//                        navRegister.navigateFrag(LoginFragment(), false)
+//                    } else {
+//                        //Toast.makeText(context, "Registration failed", Toast.LENGTH_LONG).show()
+//                        if (!Patterns.EMAIL_ADDRESS.matcher(mUsername.text.toString()).matches()) {
+//                            mUsername.error = "Please enter a valid email address"
+//                        } else if (mPassword.text.toString() != mConfirmPassword.text.toString()) {
+//                             mConfirmPassword.error = "passwords do not match"
+//                        }
+//                    }
+//                }
+
+//                if (!Patterns.EMAIL_ADDRESS.matcher(mUsername.text.toString()).matches()) {
+//                    mUsername.error = "Please enter a valid email address"
+//                } else if (mPassword.text.toString() != mConfirmPassword.text.toString()) {
+//                     mConfirmPassword.error = "passwords do not match"
+//                } else {
+//                    Toast.makeText(context, "You are registered!", Toast.LENGTH_SHORT).show()
+//                }
